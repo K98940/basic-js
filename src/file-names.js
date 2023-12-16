d381538376ffea8c5b456bdedbcbe92514adabbc
k98940 <pv1xxb@yandex.ru>
2023-12-16 19:15:39 +0600

    feat: matrix, mac, email, file-names. (Sat, Dec 16, 2023 07:15:39 PM)

--- src/file-names.js
+++ src/file-names.js
@@ -15,9 +15,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * the output should be ["file", "file(1)", "image", "file(1)(1)", "file(2)"]
  *
  */
-function renameFiles(/* names */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function renameFiles(names) {
+  const newNames = {};
+  names.forEach(name => {
+    if (!newNames[name]) {
+      newNames[name] = [];
+      newNames[name].push(name);
+    } else {
+      newNames[name].push(name);
+      const count = newNames[name].length - 1;
+      newNames[`${name}(${count})`] = [];
+      newNames[`${name}(${count})`].push(name);
+    }
+  });
+
+  return Object.keys(newNames)
 }
 
 module.exports = {
--- src/mac-address.js
+++ src/mac-address.js
@@ -14,9 +14,13 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function isMAC48Address(n) {
+  const blocks = n.split('-');
+  return blocks.reduce((isMAC, block) => {
+    if (!isMAC) return false;
+    const num = parseInt(block, 16);
+    return num >= 0 && num <= 255
+  }, true)
 }
 module.exports = {
   isMAC48Address
